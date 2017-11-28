247ae55441e4ded6d8a4eae4fdc91624d0eea0d6
FullFaceRub <danielhbury@gmail.com>
2017-11-28 09:49:52 -0700

    made a change to first problem

--- practice.js
+++ practice.js
@@ -6,7 +6,12 @@
 
 //Create an object called me. Give it a key of name with the value being your name, and another key of age with the value being your age. Then alert your name using dot notation.
 
-  //Code here
+var me = {
+  name:"Dan",
+  age:28
+}
+me.name;
+//Code here
 
 
 
